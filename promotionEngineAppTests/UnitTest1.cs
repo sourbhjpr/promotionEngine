7b9c918d65adbad1c5af51cdf189b08023012900
sourabh.agrawal@decurtis.com <sourabh.agrawal@decurtis.com>
2020-06-14 18:34:41 +0530

    First Commit

---
+++ promotionEngineAppTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace promotionEngineAppTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
